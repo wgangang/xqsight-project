ccf63f139f2429f4ec569e421705916c4692a4f7
ganggang.wang <ganggang.wang@qiaofang.cn>
2020-04-30 22:16:38 +0800

    udpate

---
+++ xqsight-parent-api/xqsight-account-api/src/main/java/com/xqsight/api/account/client/UserInfoClient.java
@@ -0,0 +1,12 @@
+package com.xqsight.api.account.client;
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈功能详细描述〉
+ *
+ * @author ganggang.wang
+ * @see [相关类/方法]（可选）
+ * @since 2020/4/30
+ */
+public interface UserInfoClient {
+}
